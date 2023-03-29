59e8f213d8ed3bf325e4e5cf4d19684bcd5c2504
Mdr060788 <maurodanielrossi3@gmail.com>
2023-03-29 09:39:45 -0300

    modificación app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 import pickle
-from PIL import Imagecle
+from PIL import Image
 #from sklearn import svm
 
 
